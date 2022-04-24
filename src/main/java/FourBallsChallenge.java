055e78fa158e4a857c682f6bdc8d7236306089e4
atchaya <atchu@dd.com>
2022-04-24 11:32:08 +0530

    Applying oops

--- src/main/java/FourBallsChallenge.java
+++ src/main/java/FourBallsChallenge.java
@@ -1,44 +1,69 @@
 import processing.core.PApplet;
 
+
+
 public class FourBallsChallenge extends PApplet {
 
 
-        public static final int WIDTH=640;
-        public static final int HEIGHT=480;
-        public static final int DIAMETER=20;
-        public static int noOfBalls=4;
-        public static  int speedOfBalls[] ={0,0,0,0};
+    public static final int WIDTH = 640;
+    public static final int HEIGHT = 480;
 
+    public static final int Diameter=30;
 
+    public static int noOfBalls = 4;
+    public static  int x[] ={0,0,0,0};
+
+    Balls[] balls = new Balls[noOfBalls];
 
     public static void main(String[] args) {
         PApplet.main("FourBallsChallenge", args);
     }
 
-        @Override
-        public void settings() {
-            super.settings();
-            size(WIDTH, HEIGHT);
-        }
+    @Override
+    public void settings() {
+        super.settings();
+        size(WIDTH, HEIGHT);
+    }
 
-        @Override
-        public void setup() {
-            paintWhite();
-        }
-
-        @Override
-        public void draw() {
-            int ball;
-            for(ball = 1; ball<=noOfBalls; ball++ ) {
-
-                ellipse(speedOfBalls[ball-1]+=ball, (HEIGHT * ball) / 5, DIAMETER, DIAMETER);
-            }
-        }
-
-        private void paintWhite() {
-            background(255);
+    @Override
+    public void setup() {
+        for(int i=1;i<=noOfBalls;i++){
+            this.balls[i - 1]=new Balls(0,(i*HEIGHT)/5, DIAMETER,i);
         }
     }
+
+    @Override
+    public void draw() {
+        for (int i = 1; i <= noOfBalls; i++) {
+            this.balls[i-1].speedOfBalls();
+            this.balls[i-1].draw(this);
+        }
+    }
+
+
+}
+ class Balls extends PApplet {
+    int X,Y, diameter,speed;
+
+    Balls(int X,int Y,int diameter,int speed) {
+        this.X=X;
+        this.Y=Y;
+        this.diameter=diameter;
+        this.speed=speed;
+    }
+
+     public void draw(FourBallsChallenge fourBallsChallenge) {
+        fourBallsChallenge.ellipse(X,Y,diameter,diameter);
+     }
+
+     public void speedOfBalls() {
+         this.X += this.speed;
+     }
+
+ }
+
+
+
 
 
 
